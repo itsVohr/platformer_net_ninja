3eabf3c2e7ceb027d637c0e137b1adadd4919c26
itsVohr <itsvohr@proton.me>
2024-05-29 04:17:27 +0200

    implement game over
    - add shitty game over scene
    - improve bullet spawn location

--- scenes/level.gd
+++ scenes/level.gd
@@ -2,10 +2,21 @@ extends Node2D
 
 const bullet_scene := preload("res://scenes/bullet.tscn")
 
+@onready var game_over_scene = preload("res://scenes/game_over.tscn")
+
+func _ready():
+	$Player.player_died.connect(_on_player_died)
+
 func _on_player_shoot(pos: Vector2, facing_right: bool):
 	var bullet  = bullet_scene.instantiate()
 	var direction = 1 if facing_right else -1
-	var bullet_offset = Vector2(14 * direction,0)
-	bullet.position = pos + bullet_offset
+	$Player.bullet_offset = direction
+	bullet.position = pos + Vector2(direction * 8, 2)
 	bullet.direction = direction
 	$Bullets.add_child(bullet)
+
+func _on_player_died():
+	call_deferred("game_over")
+	
+func game_over():
+	get_tree().change_scene_to_packed(game_over_scene)
--- scenes/player.gd
+++ scenes/player.gd
@@ -10,6 +10,8 @@ var health := 100
 var vulnerable: = true
 
 signal shoot(pos: Vector2)
+signal player_died
+
 
 func _ready():
 	$Timers/FireTimer.timeout.connect(_on_fire_timer_timeout)
@@ -24,10 +26,8 @@ func _process(_delta):
 	get_facing_direction()
 	if is_on_floor():
 		can_double_jump = true
-	
 	velocity.x = direction_x * speed
 	move_and_slide()
-	check_death()
 
 func get_input():
 	direction_x = Input.get_axis("left", "right")
@@ -36,7 +36,6 @@ func get_input():
 		$Sounds/JumpSound.play()
 		if not is_on_floor():
 			can_double_jump = false
-			$Timers/JumpCooldown
 	if Input.is_action_just_pressed("shoot") and can_shoot and has_gun:
 		shoot.emit(global_position, facing_right)
 		can_shoot = false
@@ -76,7 +75,8 @@ func get_damage(amount: int):
 		vulnerable = false
 		$Timers/InvulnerabilityTimer.start()
 		$Sounds/DamageSound.play()
+		check_death()
 		
 func check_death():
 	if health <= 0:
-		get_tree().quit()
+		player_died.emit()
